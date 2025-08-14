3ce7511df9c02995aa027d870e21797edbf15774
To Xuan Lam <to.xuan.lam@styl.solutions>
2025-08-14 13:48:55 +0700

    fix fileName use incorrect path separator

--- lib/src/file_system_entity_ext.dart
+++ lib/src/file_system_entity_ext.dart
@@ -3,5 +3,5 @@ import 'dart:io';
 import 'string_ext.dart';
 
 extension FileSystemEntityExt on FileSystemEntity {
-  String get fileName => path.substringAfterLast("/", "");
+  String get fileName => path.substringAfterLast(Platform.pathSeparator, "");
 }
